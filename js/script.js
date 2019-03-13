d38423b5d354fb2c12b4b506b2dca8a306e28038
Wilyam <WilyamNosov@users.noreply.github.com>
2019-03-13 13:32:02 +0200

    CreateProject

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var table = document.querySelector('tbody');
+
+var requestJson = 'https://raw.githubusercontent.com/WilyamNosov/Web-Labs/master/toPars.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestJson);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+	var peoples = request.response;
+	var timerId = setInterval(function() {
+  		createPeople(peoples);
+	}, 2000);
+}
+
+function createPeople(jsonObj){
+	var tableLenght = document.getElementsByTagName('tr');
+	
+	var peoplesList = jsonObj['employee'];
+
+	for (var i = tableLenght.length; i < peoplesList.length; i++) {
+
+		var trElem = document.createElement('tr');
+		var tdId = document.createElement('td');
+		var tdFirstName = document.createElement('td');
+		var tdLastdName = document.createElement('td');
+
+		tdId.textContent = peoplesList[i].id;
+		tdFirstName.textContent = peoplesList[i].firstName;
+		tdLastdName.textContent = peoplesList[i].lastName;
+
+		trElem.appendChild(tdId);
+		trElem.appendChild(tdFirstName);
+		trElem.appendChild(tdLastdName);
+		trElem.appendChild(tdId);
+
+		table.appendChild(trElem);
+	}
+}
