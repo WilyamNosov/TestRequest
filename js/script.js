ce3b9e098dc3e7a09f501f3b74df5fb588cf252d
Wilyam <WilyamNosov@users.noreply.github.com>
2019-04-08 16:24:33 +0300

    update

--- js/script.js
+++ js/script.js
@@ -37,19 +37,3 @@ function createPeople(jsonObj){
 	}
 	request.abort();
 }
-
-
-function getAllElementsWithAttribute(attribute)
-{
-  var matchingElements = [];
-  var allElements = document.getElementsByTagName('*');
-  for (var i = 0, n = allElements.length; i < n; i++)
-  {
-    if (allElements[i].getAttribute(attribute) !== null)
-    {
-      // Element exists with attribute. Add to array.
-      matchingElements.push(allElements[i]);
-    }
-  }
-  return matchingElements;
-}
